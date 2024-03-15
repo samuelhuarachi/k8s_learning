380ba23248a6c9497a318d34ad1739858264e309
Samuel Gomes <samuel.huarachi@gmail.com>
2024-03-15 17:48:57 -0300

    config maps e volumes

--- server.go
+++ server.go
@@ -3,9 +3,12 @@ package main
 import "net/http"
 import "os"
 import "fmt"
+import "io/ioutil"
+import "log"
 
 func main() {
-	http.HandleFunc("/", Hello)
+	http.HandleFunc("/configmap", ConfigMap)
+	http.HandleFunc("/", Hello)	
 	http.ListenAndServe(":8000", nil)
 }
 
@@ -13,4 +16,12 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	name := os.Getenv("NAME")
 	age := os.Getenv("AGE")
 	fmt.Fprintf(w, "Hello, I'm %s. I'm %s.", name, age)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile("myfamily/family.txt")
+	if err != nil {
+		log.Fatalf("Error reading file: ", err)
+	}
+	fmt.Fprintf(w, "My family: %s.", string(data))
 }
