90d20ccfbde30ff1d162497cad4ae17fc3c9724a
Samuel Gomes <samuel.huarachi@gmail.com>
2024-03-18 21:06:38 -0300

    secrets

--- server.go
+++ server.go
@@ -7,6 +7,7 @@ import "io/ioutil"
 import "log"
 
 func main() {
+	http.HandleFunc("/secret", Secret)
 	http.HandleFunc("/configmap", ConfigMap)
 	http.HandleFunc("/", Hello)	
 	http.ListenAndServe(":8000", nil)
@@ -16,6 +17,12 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	name := os.Getenv("NAME")
 	age := os.Getenv("AGE")
 	fmt.Fprintf(w, "Hello, I'm %s. I'm %s.", name, age)
+}
+ 
+func Secret(w http.ResponseWriter, r *http.Request) {
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+	fmt.Fprintf(w, "User %s, password %s.", user, password)
 }
 
 func ConfigMap(w http.ResponseWriter, r *http.Request) {
